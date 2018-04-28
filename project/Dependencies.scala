068983c6bbf06deac49c2bf13d5703e66e2658f1
Elijah Rippeth <erippeth@mitre.org>
2018-04-28 11:53:20 -0400

    remove redundant version for Enumeratum.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,15 +3,13 @@ import sbt._
 object Dependencies {
 
   private object Versions {
-    val Enumeratum = "1.5.12"
-    val EnumeratumPlayJson = "1.5.12-2.6.0-M7"
-
+    val Enumeratum = "1.5.13"
     val ScalaLogging = "3.7.2"
     val ScalaTest = "3.0.5"
   }
 
   private val enumeratum = "com.beachape" %% "enumeratum" % Versions.Enumeratum
-  private val enumeratumPlayJson = "com.beachape" %% "enumeratum-play-json" % Versions.EnumeratumPlayJson
+  private val enumeratumPlayJson = "com.beachape" %% "enumeratum-play-json" % Versions.Enumeratum
 
   private val scalatest = "org.scalatest" %% "scalatest" % Versions.ScalaTest % "test"
 
@@ -20,5 +18,4 @@ object Dependencies {
     enumeratumPlayJson,
     scalatest
   )
-
 }
